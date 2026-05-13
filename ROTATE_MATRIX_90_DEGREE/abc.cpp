68a4f0386599ce70a6ea5e23039d04f2d8f5496d
PalavalasaSaiJoshitha <322103382046@gvpce.ac.in>
2026-05-13 13:36:36 +0530

    Updated rotate matrix by 90 , a neetcode problem

--- ROTATE_MATRIX_90_DEGREE/abc.cpp
+++ ROTATE_MATRIX_90_DEGREE/abc.cpp
@@ -1,1 +1,36 @@
-hii
+//Rotate image by 90 degree
+#include<bits/stdc++.h>
+using namespace std;
+void rotatematrix(vector<vector<int>> &mat){
+    int n=mat.size();
+for(int i=0;i<=n-2;i++){
+    for(int j=i+1;j<=n-1;j++){
+        swap(mat[i][j], mat[j][i]);
+    }
+}
+
+for(int i=0;i<n;i++){
+    reverse(mat[i].begin(), mat[i].end());
+}
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<vector<int>> mat(n, vector<int> (n));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>mat[i][j];
+        }
+    }
+    rotatematrix(mat);
+ for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<mat[i][j];
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
