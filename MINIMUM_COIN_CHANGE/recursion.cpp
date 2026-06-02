f082d8fb6b4fe55dc05a5cacb86d418bb1a4f476
PalavalasaSaiJoshitha <322103382046@gvpce.ac.in>
2026-06-02 13:34:13 +0530

    Written recusriosn code for minimum coin change

---
+++ MINIMUM_COIN_CHANGE/recursion.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+int f(vector<int> &arr, int ind, int T){
+    if(ind==0){
+    if(T%arr[0]==0){
+        return T/arr[0];
+    }
+    else{
+        return 1e9;
+    }
+    int notpick=0+f(arr,ind-1,T);
+    int pick=INT_MAX;
+    if(arr[ind]<=T){
+        pick=1+f(arr,ind,T-arr[ind]);
+    }
+    return min(pick,notpick);
+}
+}
+
+int minimumelements(vector<int> &arr,int T){
+    int n=arr.size();
+    int ans= f(arr,n-1,T);
+    if(ans>=1e9){
+        return -1;
+    }
+    else{
+        return ans;
+    }
+}
+int main(){
+    vector<int> arr;
+    for(int i=0;i<arr.size();i++){
+        cin>>arr[i];
+
+    }
+    int target;
+    cin>>target;
+
+    int ans=minimumelements(arr,target);
+    cout<<ans<<endl;
+}
