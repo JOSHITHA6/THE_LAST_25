c606bc67fa1acd47b2394d2572eec86e068664e7
PalavalasaSaiJoshitha <322103382046@gvpce.ac.in>
2026-06-11 12:19:45 +0530

    Executed meeting room -1 , lc premium, blind75

---
+++ MEETING_ROOM/abc.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool meetingroom(vector<vector<int>> &intervals){
+    sort(intervals.begin(), intervals.end());
+    for(int i=1;i<intervals.size();i++){
+        if(intervals[i][0]<intervals[i-1][1])
+        return false;
+    }
+    return true;
+}
+
+int main(){
+    int n;
+
+    cout<<"Enter no of rows:";
+
+    vector<vector<int>> intervals(n, vector<int> (2));
+    for(int i=0;i<intervals.size();i++){
+        cout<<"Enter start and end time of " << i+1 <<" meeting: ";
+        cin>>intervals[i][0]>>intervals[i][1];
+    }
+    cout<<meetingroom(intervals)?"true":"false";
+    
+}
